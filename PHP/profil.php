7c4fc4b2f4610dc9837b7d1f24f864f991b16cc9
Awizz <ruben.penalvez@gmail.com>
2024-05-14 14:46:58 +0200

    themes profil.php

--- PHP/profil.php
+++ PHP/profil.php
@@ -370,3 +370,12 @@ if (isset($_SESSION["LOGGEDIN"])) {
                         </h2>
                     </div>
                 </div>
+                <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+                <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js"
+                    integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp"
+                    crossorigin="anonymous"></script>
+                <script src="./assets/dashboard.js"></script>
+</body>
+
+</html>
