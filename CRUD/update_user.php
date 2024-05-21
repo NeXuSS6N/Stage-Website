d76e458f8c04f019a73c414045464e3ac193355f
Awizz <ruben.penalvez@gmail.com>
2024-05-21 11:30:41 +0200

    Update valeurs a vide

--- CRUD/update_user.php
+++ CRUD/update_user.php
@@ -9,6 +9,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $new_email = $_POST['new_email'];
     $new_password = $_POST['new_password'];
 
+    // Récupère les valeurs actuelles de l'utilisateur
+    $sqlGetUser = "SELECT J_Username, J_Mail, J_Mdp FROM account WHERE J_ID = ?";
+    if ($stmt = mysqli_prepare($conn, $sqlGetUser)) {
+        mysqli_stmt_bind_param($stmt, "i", $user_id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+        mysqli_stmt_bind_result($stmt, $current_username, $current_email, $current_password);
+        mysqli_stmt_fetch($stmt);
+        mysqli_stmt_close($stmt);
+    }
+
+    // Vérifie et remplace les valeurs vides par les valeurs actuelles
+    $new_username = ($new_username === '') ? $current_username : $new_username;
+    $new_email = ($new_email === '') ? $current_email : $new_email;
+    $new_password = ($new_password === '') ? $current_password : $new_password;
+
     // Prépare une déclaration SQL avec des paramètres liés
     $sqlQuery = "UPDATE account SET J_Username = ?, J_Mail = ?, J_Mdp = ? WHERE J_ID = ?";
     if ($stmt = mysqli_prepare($conn, $sqlQuery)) {
