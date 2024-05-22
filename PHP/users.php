40ded698b3d622f3e7692a2ab134bfb8df51d616
Awizz <ruben.penalvez@gmail.com>
2024-05-22 10:33:34 +0200

    balises <br> pour meilleur visuel du crud

--- PHP/users.php
+++ PHP/users.php
@@ -25,6 +25,7 @@ foreach ($resultat as $resultats) {
 
   <div class="user-content">
     <h5>
+      <br>
       <?php echo $resultats['J_Username'] ?>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <?php echo $resultats['J_Mail'] ?>
@@ -37,11 +38,13 @@ foreach ($resultat as $resultats) {
       <input type="text" name="new_username" placeholder="Nouveau nom d'utilisateur">
       <input type="email" name="new_email" placeholder="Nouvelle adresse e-mail">
       <input type="password" name="new_password" placeholder="Nouveau mot de passe">
+      <br>
       <button type="submit">Modifier</button>
     </form>
     <!-- Formulaire pour supprimer l'utilisateur -->
     <form method="post" action="../CRUD/delete_user.php">
       <input type="hidden" name="user_id" value="<?php echo $resultats['J_Id'] ?>">
+      <br>
       <button type="submit">Supprimer</button>
     </form>
   </div>
@@ -50,10 +53,14 @@ foreach ($resultat as $resultats) {
 ?>
 <div class="user-content">
   <!-- Formulaire pour ajouter un nouvel utilisateur -->
+  <br>
+  <br>
   <form method="post" action="../CRUD/add_user.php">
     <input type="text" name="username" placeholder="Nom d'utilisateur">
     <input type="email" name="email" placeholder="Adresse e-mail">
     <input type="password" name="password" placeholder="Mot de passe">
+    <br>
     <button type="submit">Ajouter utilisateur</button>
+    <br>
   </form>
 </div>
